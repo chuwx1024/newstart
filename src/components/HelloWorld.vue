82bb775e5466cc08976160034a552eaa74930b93
chuhx <chuhx1024@126.com>
2021-03-10 14:52:39 +0800

    HelloWorld.vue组件修改了一行代码

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>这是chuhx添加的代码</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
