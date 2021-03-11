fa55480a9e6056210a09969e94bb4b57e5bd6521
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-03-11 14:54:46 +0800

    second

---
+++ src/components/douya.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-baoxincai"></use>
+      </svg>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-lvdouya"></use>
+      </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DouYa'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/font_95liwwld5cd/iconfont'
+import './assets/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
