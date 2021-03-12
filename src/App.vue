cb9d54c6b26194c974201a1fd2ebbadfd0ca710d
chuhx <chuhx1024@126.com>
2021-03-12 10:10:43 +0800

    添加备注

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-    <div id="app">
-        <router-view />
-    </div>
+  <div id="app">
+    <!-- 这里是路由放置的坑------------------------------ -->
+    <router-view />
+    <!-- 这里是路由放置的坑------------------------------ -->
+  </div>
 </template>
 
 <script>
