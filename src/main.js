964de136f9967fb9fc699d28597057e2a9002a7f
chuhx <chuhx1024@126.com>
2021-03-12 09:16:07 +0800

    修改 iconfont 和 全局 样式文件的位置

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import './assets/font_95liwwld5cd/iconfont'
-import './assets/index.css'
+
+// 引入 iconFont
+import './assets/iconFont/iconfont'
+import './assets/iconFont/icon.css'
+
+// 引入全局样式
+import '@/assets/style/index.less'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
