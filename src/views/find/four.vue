2cb7bd0e603f0df19cf3681743d8f7326dab345b
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-03-16 13:43:40 +0800

    detailpage

--- src/views/find/four.vue
+++ src/views/find/four.vue
@@ -1,47 +1,41 @@
 <template>
   <el-card class="box-card">
     <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="数据名称">
-        <el-input v-model="form.dataname"></el-input>
+      <el-form-item label="数据名称 :">
+        <el-input placeholder="请输入" v-model="form.dataname"></el-input>
+        <span>回电话呵呵呵</span>
       </el-form-item>
-      <el-form-item label="活动区域">
+      <el-form-item label="数据描述 :">
+        <el-input placeholder="请输入" type="textarea" v-model="form.desc"></el-input>
+      </el-form-item>
+      <el-form-item label="数据标签 :">
         <el-select v-model="form.region" placeholder="请选择活动区域">
           <el-option label="区域一" value="shanghai"></el-option>
           <el-option label="区域二" value="beijing"></el-option>
         </el-select>
+        <span>标签最多设置30个</span>
       </el-form-item>
-      <el-form-item label="活动时间">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-        </el-col>
+      <el-form-item label="特征分类 :">
+        <el-input placeholder="性别，年龄..." type="textarea" v-model="form.desc"></el-input>
       </el-form-item>
-      <el-form-item label="即时配送">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
+      <el-form-item label="开放范围 :">
+        <el-radio-group v-model="form.radio">
+          <el-radio :label="3">全部用户可见</el-radio>
+          <el-radio :label="6">对部分用户可见</el-radio>
+          <el-radio :label="9">对部分用户不可见</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="活动形式">
-        <el-input type="textarea" v-model="form.desc"></el-input>
+      <el-form-item>
+        <el-input
+          placeholder="输入企业名称"
+          v-model="input"
+          clearable>
+        </el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
+        <el-button size="small"  @click="onSubmit">取消</el-button>
+        <el-button size="small" type="primary" @click="onSubmit">保存</el-button>
+        <el-button size="small" type="primary" @click="onSubmit">保存并发布</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -59,7 +53,8 @@ export default {
         delivery: false,
         type: [],
         resource: '',
-        desc: ''
+        desc: '',
+        radio: 6
       }
     }
   },
@@ -70,3 +65,14 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="less">
+span {
+  font-size: 12px;
+  color: #999;
+  margin-top: -10px;
+  margin-bottom: -20px;
+  display: block;
+}
+
+</style>
